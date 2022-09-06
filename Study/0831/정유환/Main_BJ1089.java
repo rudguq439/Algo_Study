e93ff55037b88b99d90213d33a8df9e1292d621e
YHJzw <wind9481@naver.com>
2022-09-06 22:10:02 +0900

    Add files via upload

---
+++ Study/0831/정유환/Main_BJ1089.java
@@ -0,0 +1,15 @@
+package weeka12_0829;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_BJ1089 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		char[][] bolb ;
+	}
+
+}
